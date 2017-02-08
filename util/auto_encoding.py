b8bc9f2e48ca03b9a00aff785ea5465dbfdf96a9
KangWon Lee <kangwon_fwd@naver.com>
2017-02-08 21:27:08 +0900

    main() : added header_str

--- util/auto_encoding.py
+++ util/auto_encoding.py
@@ -1,8 +1,14 @@
+# coding: utf-8
 import os
 
 
 def main():
-    pass
+    header_str = '''# -*- coding: utf8 -*-
+# 위 주석은 이 .py 파일 안에 한글이 사용되었다는 점을 표시하는 것임
+# [학번] [이름]
+# 각 행의 자세한 주석을 추가하시오
+'''
+    print(header_str)
 
 
 if __name__ == '__main__':
