868ea215a5b32d641864ed31ee42042e12fc47be
KangWon Lee <kangwon_fwd@naver.com>
2017-02-08 23:47:58 +0900

    process_path() : use read_lines() instead of .read() and .splitlines() added write_lines() using .writelines()

--- util/auto_encoding.py
+++ util/auto_encoding.py
@@ -37,17 +37,28 @@ def recursively_add_header(path):
 def process_path(root, filenames, header_str):
     print("* root = %s" % root)
     for filename in filenames:
+        b_rewrite = False
         full_path = os.path.join(root, filename)
         with open(full_path, 'r') as f_in:
-            txt = f_in.read()
+            txt_lines = f_in.readlines()
             f_in.close()
-        txt_lines = txt.splitlines()
+
+        original_length = len(txt_lines)
 
         # if the first line includes coding, remove it
         if is_line_encoding(txt_lines[0]):
             txt_lines.pop(0)
+            b_rewrite = True
 
-        print("** full_path = %s : %d" % (full_path, len(txt)))
+        if b_rewrite:
+            print("** full_path = %s : %d -> %d" % (full_path, original_length, len(txt_lines)))
+            write_lines(full_path, txt_lines)
+
+
+def write_lines(full_path_string, lines):
+    with open(full_path_string, 'wt') as f_out:
+        f_out.writelines(lines)
+        f_out.close()
 
 
 def is_line_encoding(line):
